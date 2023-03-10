673ab5d2d1ab6b1d735d24c69462fc7e4689e066
onlyoranje <31823747+onlyoranje@users.noreply.github.com>
2023-03-10 16:03:46 +0300

    2601

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,5 @@
 import './bootstrap'
-import * as mdb from 'mdb-ui-kit'
-import { Input } from 'mdb-ui-kit'
+
 import '../sass/app.scss'
 import "./image-uploader"
 import "./jquery.fileuploader.min"
