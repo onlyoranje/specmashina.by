d6fceba841ff84b99c96e7a46bc02abdb9033eeb
onlyoranje <ermakov_maksim@mail.ru>
2023-12-18 21:03:13 +0300

    kljkl;j

--- app/Models/UserFile.php
+++ app/Models/UserFile.php
@@ -34,7 +34,7 @@ class UserFile extends Model
         if (!file_exists(Storage::path('/public/').'thumbnails/'.$w.'x'.$h.'/'.$this->url)){
             $save_path= Storage::path('/public/').'thumbnails/'.$w.'x'.$h.'/';
             if (!file_exists($save_path)) {
-                mkdir($save_path, 666, true);
+                mkdir($save_path, 755, true);
             }
             $thumbnail = Image::make(Storage::path('/public/').$this->url);
             $thumbnail->fit($w, $h);
