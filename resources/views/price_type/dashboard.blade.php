1ec327b1f2e070555f26d6de37fbebef4e3a5b34
ermak <ermakov_maksim@mail.ru>
2024-02-14 21:16:20 +0300

    0224

--- resources/views/price_type/dashboard.blade.php
+++ resources/views/price_type/dashboard.blade.php
@@ -54,7 +54,7 @@
 
 
 
-    <div class="py-12">
+   {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
@@ -94,6 +94,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>--}}
 @endsection
 
