d09af243c6c594262b8dd60fa2f191ec0c89c629
onlyoranje <31823747+onlyoranje@users.noreply.github.com>
2024-04-26 14:29:38 +0300

    страница rubric виджеты для страницы rubric

--- app/Http/Controllers/ImsController.php
+++ app/Http/Controllers/ImsController.php
@@ -119,29 +119,29 @@ class ImsController extends Controller
         $dialog = [];
         $user2=substr($id,2);
         $all_messages = Im::whereIn('user1_id', [Auth::id(),$user2])->whereIn('user2_id', [Auth::id(),$user2])->orderby('created_at','asc')->get();
+
         foreach ($all_messages as $message)
         {
-            if ($message->user1_id==Auth::id()) {
-                $user2 = $message->user2_id;
-                $user1 = $message->user1_id;
-            }
-            else{
-                $user2 = $message->user1_id;
-                $user1 = $message->user2_id;
-                if (!isset($message->read_at))
-                {
-                    if (isset($dialog['D_'.$user2]['unread']))
-                        $dialog['D_'.$user2]['unread']++;
-                    else
-                        $dialog['D_'.$user2]['unread']=1;
+            if ($message->user1_id!=$message->user2_id) {
+                if ($message->user1_id == Auth::id()) {
+                    $user2 = $message->user2_id;
+                    $user1 = $message->user1_id;
+                } else {
+                    $user2 = $message->user1_id;
+                    $user1 = $message->user2_id;
+                    if (!isset($message->read_at)) {
+                        if (isset($dialog['D_' . $user2]['unread']))
+                            $dialog['D_' . $user2]['unread']++;
+                        else
+                            $dialog['D_' . $user2]['unread'] = 1;
+                    }
                 }
+                $dialog['D_' . $user2]['updated_at'] = $message->updated_at;
+                $dialog['D_' . $user2]['message'][] = $message;
+                $dialog['D_' . $user2]['last_message'] = Str::limit(strip_tags($message->text), 50);
+                if (!isset($dialog['D_' . $user2]['user1'])) $dialog['D_' . $user2]['user1'] = User::where('id', $user1)->first();
+                if (!isset($dialog['D_' . $user2]['user2'])) $dialog['D_' . $user2]['user2'] = User::where('id', $user2)->first();
             }
-            $dialog['D_'.$user2]['updated_at'] = $message->updated_at;
-            $dialog['D_'.$user2]['message'][] = $message;
-            $dialog['D_'.$user2]['last_message'] = Str::limit(strip_tags($message->text),50);
-            if (!isset($dialog['D_'.$user2]['user1'])) $dialog['D_'.$user2]['user1'] = User::where('id',$user1)->first();
-            if (!isset($dialog['D_'.$user2]['user2'])) $dialog['D_'.$user2]['user2'] = User::where('id',$user2)->first();
-
         }
 
         uasort($dialog, function ($a, $b)
--- routes/web.php
+++ routes/web.php
@@ -129,8 +129,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/search', [App\Http\Controllers\SearchController::class, 'search_result'])->name('search');
 
 Route::get('/messages', [App\Http\Controllers\ImsController::class, 'index'])->middleware(['auth', 'verified'])->name('messages');
-Route::get('/chats', [App\Http\Controllers\ImsController::class, 'chats'])->middleware(['auth', 'verified'])->name('chats');
 Route::get('/chats/{id}', [App\Http\Controllers\ImsController::class, 'chat'])->middleware(['auth', 'verified'])->name('chat');
+Route::get('/chats', [App\Http\Controllers\ImsController::class, 'chats'])->middleware(['auth', 'verified'])->name('chats');
+
 Route::patch('/read_message/{id}', [App\Http\Controllers\ImsController::class, 'read_msg'])->middleware(['auth', 'verified'])->name('read_msg');
 Route::patch('/read_alert/{id}', [App\Http\Controllers\NotificationController::class, 'read_alert'])->middleware(['auth', 'verified'])->name('read_msg');
 Route::patch('/new_msg', [App\Http\Controllers\ImsController::class, 'new_msg'])->middleware(['auth', 'verified'])->name('new_msg');
