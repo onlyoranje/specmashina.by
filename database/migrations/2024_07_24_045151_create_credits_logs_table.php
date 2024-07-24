08136c138471d8f31c73d785f0d95a6deb1e8d26
onlyoranje <31823747+onlyoranje@users.noreply.github.com>
2024-07-24 07:59:52 +0300

    страница rubric виджеты для страницы rubric npm install --save @ckeditor/ckeditor5-build-classic

---
+++ database/migrations/2024_07_24_045151_create_credits_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credits_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('credits')->nullable();
+            $table->unsignedBigInteger('bb_id');
+            $table->string('description');
+            $table->foreign('bb_id')->references('id')->on('bbs')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('credits_logs');
+    }
+};
